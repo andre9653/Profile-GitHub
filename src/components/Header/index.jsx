1ef1bdd1e8af32fb87a3d0d66f9581d5bfb5f139
andre9653 <andresena9653@gmail.com>
2022-01-06 19:57:37 -0300

    Primeiros testes de codigos

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      {/* name
+     link github */}
+      hello world
+    </header>
+  );
+}
+
+export default Header;
